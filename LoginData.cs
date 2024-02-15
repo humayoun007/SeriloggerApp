49bcb8955e47acffa1a681856284e2ad639f98df
Md. Humayoun Kabir <humayounkabir30@gmail.com>
2024-02-15 17:07:01 +0600

    Add files via upload

---
+++ LoginData.cs
@@ -0,0 +1,7 @@
+namespace MYLOGGERAPP;
+public class LoginData
+{
+    public string? Username;
+    // ReSharper disable once NotAccessedField.Global
+    public string? Password;
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿﻿using Microsoft.Extensions.Configuration;
+using MYLOGGERAPP;
+using Serilog;
+using Serilog.Core;
+using Serilog.Debugging;
+
+SelfLog.Enable(Console.Error);
+
+Thread.CurrentThread.Name = "Main thread";
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+logger.Information("Args: {Args}", args);
+
+do
+{
+    logger.ForContext<Program>().Information("Hello, world!");
+    logger.ForContext<Program>().Error("Hello, world!");
+    logger.ForContext(Constants.SourceContextPropertyName, "Microsoft").Warning("Hello, world!");
+    logger.ForContext(Constants.SourceContextPropertyName, "Microsoft").Error("Hello, world!");
+    logger.ForContext(Constants.SourceContextPropertyName, "MyApp.Something.Tricky").Verbose("Hello, world!");
+
+    logger.Information("Destructure with max object nesting depth:\n{@NestedObject}",
+        new { FiveDeep = new { Two = new { Three = new { Four = new { Five = "the end" } } } } });
+
+    logger.Information("Destructure with max string length:\n{@LongString}",
+        new { TwentyChars = "0123456789abcdefghij" });
+
+    logger.Information("Destructure with max collection count:\n{@BigData}",
+        new { TenItems = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" } });
+
+    logger.Information("Destructure with policy to strip password:\n{@LoginData}",
+        new LoginData { Username = "BGates", Password = "isityearoflinuxyet" });
+
+    Console.WriteLine("\nPress \"q\" to quit, or any other key to run again.\n");
+}
+while (!args.Contains("--run-once") && (Console.ReadKey().KeyChar != 'q'));
